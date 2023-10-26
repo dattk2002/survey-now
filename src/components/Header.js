672190f8a81794c572c43b128a1557fb64f47dd4
kimdat0705 <kimdat0705@gmail.com>
2023-10-26 20:02:49 +0700

    Dat finish landing page

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,26 +4,38 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
-import Avatar from "@mui/material/Avatar";
+// import Avatar from "@mui/material/Avatar";
+import { Avatar } from "antd";
+import {
+  DownOutlined,
+  SettingOutlined,
+  QuestionCircleOutlined,
+  ProfileOutlined,
+} from "@ant-design/icons";
+import { Dropdown, Space } from "antd";
+import { Menu } from "antd";
 import { styled } from "@mui/system";
 import "../App.css";
 
 import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
 
+const url =
+  "https://i.pinimg.com/236x/8b/58/92/8b5892170b8088104ffa26ec24850dd6.jpg";
+
 //CSS component
 const CustomBottomNavigation = styled(BottomNavigation)`
   margin-left: 15%;
-  background-color: #00B14F;
+  background-color: #00b14f;
 `;
 
 const CustomBottomNavigationAction = styled(BottomNavigationAction)`
   margin: 0 10%;
   color: white;
-  padding: 0 5%;
+  padding: 0;
 `;
 
 const CustomToolbar = styled(Toolbar)`
-  background-color: #00B14F;
+  background-color: #00b14f;
 `;
 
 const CustomAvatar = styled(Avatar)`
@@ -34,12 +46,46 @@ const CustomAvatar = styled(Avatar)`
 const CustomFormControl = styled(FormControl)`
   margin-left: 1%;
   border: none;
-
 `;
 
-// const CustomFormControl:hover = styled(FormControl)`
-// margin-left: 1%;
-// `;
+const items = [
+  {
+    key: "1",
+    label: (
+      <a target="_blank" rel="noopener noreferrer">
+        Profile
+      </a>
+    ),
+    icon: <ProfileOutlined />,
+  },
+  {
+    key: "2",
+    label: (
+      <a target="_blank" rel="noopener noreferrer">
+        Settings
+      </a>
+    ),
+    icon: <SettingOutlined />,
+  },
+  {
+    key: "3",
+    label: (
+      <a target="_blank" rel="noopener noreferrer">
+        Help
+      </a>
+    ),
+    icon: <QuestionCircleOutlined />,
+  },
+  {
+    key: "4",
+    danger: true,
+    label: (
+      <a target="_blank" rel="noopener noreferrer">
+        Log Out
+      </a>
+    ),
+  },
+];
 
 const Header = () => {
   const [value, setValue] = React.useState(0);
@@ -59,35 +105,30 @@ const Header = () => {
           value={value}
           onChange={(event, newValue) => {
             setValue(newValue);
-          }}  
+          }}
         >
-          <CustomBottomNavigationAction label="Home" />
-          <CustomBottomNavigationAction label="About us" />
-          <CustomBottomNavigationAction label="New Survey" />
+          <CustomBottomNavigationAction label="Trang chủ" />
+          <CustomBottomNavigationAction label="Về chúng tôi" />
+          <CustomBottomNavigationAction label="Khảo sát" />
         </CustomBottomNavigation>
-        <CustomAvatar
-          alt="Remy Sharp"
+        {/* <CustomAvatar
+          alt="Ducki"
           src="https://i.pinimg.com/564x/04/55/2e/04552e8c76b3e5dec67bd23345b26971.jpg"
           sx={{ width: 56, height: 56 }}
-        />
-        <Typography variant="h7">Ducki</Typography>
-        <CustomFormControl variant="outlined" sx={{  }}>
-        <InputLabel id="dropdown-label"></InputLabel>
-        <Select
-          labelId="dropdown-label"
-          id="dropdown"
-          value={selectedValue}
-          onChange={handleChange}
-          label="Select an Option"
-        >
-          {/* <MenuItem value="">
-            <em>None</em>
-          </MenuItem> */}
-          <MenuItem value="option1">Profile</MenuItem>
-          <MenuItem value="option2">Settings</MenuItem>
-          <MenuItem value="option3">Sign Out</MenuItem>
-        </Select>
-      </CustomFormControl>
+        /> */}
+          <CustomAvatar
+            src={<img src={url} alt="avatar" className="avatar-image" />}
+          />
+          <Typography variant="h7">John Doe</Typography>
+          <CustomFormControl variant="outlined" sx={{}}>
+            <Dropdown overlay={<Menu items={items} />} trigger={["hover"]}>
+              <a onClick={(e) => e.preventDefault()}>
+                <Space>
+                  <DownOutlined />
+                </Space>
+              </a>
+            </Dropdown>
+          </CustomFormControl>
       </CustomToolbar>
     </AppBar>
 
